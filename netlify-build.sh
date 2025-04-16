14d0b983d077ae378133ffade67d0ed08db668bc
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2025-04-16 23:23:59 +0530

    refactor: Add post-build image copy and cleanup for cpp-notes

--- netlify-build.sh
+++ netlify-build.sh
@@ -16,4 +16,26 @@ git clone --depth 1 $CPP_REPO $CPP_COLLECTION_DIR # Use --depth 1 for faster clo
 echo "Running Jekyll build..."
 bundle exec jekyll build
 
+# --- Post-build steps ---
+echo "Copying images to the final site directory..."
+SOURCE_IMG_DIR="$CPP_COLLECTION_DIR/img-data"
+DEST_IMG_DIR="_site/notes/cpp/img-data" # The final path required for the URL
+
+if [ -d "$SOURCE_IMG_DIR" ]; then
+  echo "Source image directory '$SOURCE_IMG_DIR' found."
+  echo "Creating destination directory '$DEST_IMG_DIR'..."
+  mkdir -p "$DEST_IMG_DIR"
+  echo "Copying images from '$SOURCE_IMG_DIR' to '$DEST_IMG_DIR'..."
+  cp -r "$SOURCE_IMG_DIR"/* "$DEST_IMG_DIR"/
+  echo "Images copied successfully."
+else
+  echo "Source image directory '$SOURCE_IMG_DIR' not found. Skipping image copy."
+fi
+
+echo "Cleaning up potentially erroneous .html files in destination..."
+# Remove files like image.html.jpg, image.html.png etc. just in case
+find "$DEST_IMG_DIR" -type f -name '*.html.jpg' -o -name '*.html.jpeg' -o -name '*.html.png' -o -name '*.html.gif' -o -name '*.html.svg' -exec rm -f {} \;
+echo "Cleanup finished."
+# --- Add future post-build steps here ---
+
 echo "Build script finished."
